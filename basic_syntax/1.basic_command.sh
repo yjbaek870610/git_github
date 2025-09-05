fcbda2926ea2702e9cda9e105c7a3e0ddc503f37
yjbaek870610 <yjbaek870610@gmail.com>
2025-09-05 15:29:55 +0900

    fix

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -1,2 +1,8 @@
 # git add . 은 모든 변경(추가)사항을 staging area로 이동
 git add .
+
+# git commit은 staging area의 변경사항을 확정짓고 commit ID를 생성
+git commit -m "메세지"
+
+# 원격 repo에 main 브랜치 변경사항(commit ID)을 업로드
+git push origin main
