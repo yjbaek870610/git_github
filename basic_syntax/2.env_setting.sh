404e6cd56db2a85d777d419e4c556faf84a13d6f
yjbaek870610 <yjbaek870610@gmail.com>
2025-09-05 17:04:04 +0900

    fix

--- basic_syntax/2.env_setting.sh
+++ basic_syntax/2.env_setting.sh
@@ -36,3 +36,18 @@
     git add .
     git commit -m "first"
     git push origin master
+
+
+# 사용자 지정 방법
+    # 전역적 사용자(이름, 이메일) 지정
+    git config --global user.name "유저네임"
+    git config --global user.email "유저이메일"
+
+    # 지역적 사용자 지정
+    git config --local user.name "유저네임"
+    git config --local user.email "유저이메일"
+
+    # 사용자 정보 조회
+    git config user.name
+    git config user.email
+    git config --list
