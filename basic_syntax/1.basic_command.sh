3d1618586e841c887ace4d2c5c74f9ed3d0d553b
yjbaek870610 <yjbaek870610@gmail.com>
2025-09-10 00:01:37 +0900

    fix

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -53,3 +53,15 @@ git pull origin 브랜치명
 
 # 원격에 변경사항을 local에 가져오되 병합은 하지 않는 것
 git fetch origin main
+
+
+# git 작업 취소
+# commit 이후의 취소
+git reset head~1
+git reset head^
+
+# staging area로 까지만 취소
+git reset --soft head~1
+
+# push 이후의 취소 -> commit 메시지 작성 vi 화면으로 이동 -> :wq로 저장 후 종료
+git revert 기존의 커밋ID
