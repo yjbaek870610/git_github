8ea4c26e0a9438f6c6292ca896c304e9b966cbd7
yjbaek870610 <yjbaek870610@gmail.com>
2025-09-11 09:53:15 +0900

    fix

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -84,3 +84,15 @@ git stash apply 인덱스번호
 
 # 임시저장목록 모두삭제
 git stash clear
+
+
+# 버전명시
+git tag v1.0
+git tag -a v1.0 -m "1. 주요사항A 2. 주요사항B"
+
+# tag release 배포
+# commit push와 상관없이 태그 별도로 push
+git push origin v1.0
+
+# 태그 목록 조회
+git tag
